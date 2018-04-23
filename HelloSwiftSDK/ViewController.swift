f7f4a23c51da2836af63feb0a46f06607d51fc7b
gds21 <gds721@gmail.com>
2018-04-23 18:49:25 +0800

    加入 x86_64 i386 armv7 armv7s arm64 等架構

--- HelloSwiftSDK/ViewController.swift
+++ HelloSwiftSDK/ViewController.swift
@@ -13,10 +13,25 @@ class ViewController: UIViewController, QLiEERMobileSDKDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
-        QLiEERMobileSDK.launchMobileViewController(accessToken: "HHH", mobileSDKDelegate: self) { _,_  in
-            //
+        QLiEERMobileSDK.set(environment: .Stage)
+        if !QLiEERMobileSDK.checkTokenIsValid() {
+            QLiEERMobileSDK.launchMobileViewController(accessToken: "", mobileSDKDelegate: self, completion: { result, vc in
+                // 結果為0代表正常
+                if result == 0 {
+                    // 呼叫 start 方法的目的是：當行動點餐的 ViewController 消失後，仍能透過 Delegate 取得訂單變化，要中止的話請使用 QLiEERMobileSDK.stop()
+                    QLiEERMobileSDK.start()
+                    self.present(vc!, animated: true, completion: nil)
+                }else{
+                    print("登入有誤")
+                }
+            })
         }
+    }
+    
+    func unreadCountUpdated(latestNumber: Int) {
+        //
     }
     
     func orderWillChange(inAction: String, sourceView: UIView) {
